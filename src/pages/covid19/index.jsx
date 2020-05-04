c0b6ba5a309cfc74bd9b87ad8353731d88b3a6d6
anjenkin <andrea.r.jenkins@gmail.com>
2020-05-04 13:17:13 -0400

    added more text

--- src/pages/covid19/index.jsx
+++ src/pages/covid19/index.jsx
@@ -70,7 +70,7 @@ export default class Covid19 extends React.Component {
     <StoryLayout
       title='required title'
       introSentence='required intro'
-			contextStatement='context statement'
+      contextStatement='Nam quis nulla. Integer malesuada. In in enim a arcu imperdiet malesuada. Sed vel lectus. Donec odio urna, tempus molestie, porttitor ut, iaculis quis, sem. Phasellus rhoncus. Aenean id metus id velit ullamcorper pulvinar. Vestibulum fermentum tortor id mi. Pellentesque ipsum. Nulla non arcu lacinia neque faucibus fringilla. Nulla non lectus sed nisl molestie malesuada. Proin in tellus sit amet nibh dignissim sagittis. Vivamus luctus egestas leo. Maecenas sollicitudin. Nullam rhoncus aliquam met'
       sectionToc={this.sections}
       hwctaLink={this.props.location.pathname + '/methodologies'}
     >
