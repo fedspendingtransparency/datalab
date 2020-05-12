534fdba71ae3be3d03d3173649f6ddcfa50c8436
598403 <Forjan_Matthew@bah.com>
2020-05-12 13:39:33 -0400

    added toggle

--- src/page-sections/covid19/tracking.jsx
+++ src/page-sections/covid19/tracking.jsx
@@ -1,32 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import AccordionList from 'src/components/accordion-list/accordion-list';
 import Downloads from 'src/components/section-elements/downloads/downloads';
 import Share from 'src/components/share/share';
 
-export default class Tracking extends React.Component {
-  constructor(props) {
-    super(props);
+import { Switch } from '@material-ui/core';
+
+const Tracking = (props) => {
+  const [checked, toggleChecked] = useState(true);
+
+  const handleToggle = (e) => {
+    toggleChecked(e.target.checked)
   }
+  
+  return (
+    <>
+      <AccordionList title='Instructions'>
+        <p>Actual instructions are larger than they appear</p>
+      </AccordionList>
 
-  render = () => <>
+      <div>
+        <span>Budget Function</span>
+        <Switch checked={checked} onChange={handleToggle} color='default' />
+        <span>Agency</span>
+      </div>
 
-    <AccordionList title='Instructions'>
-      <p>Actual instructions are larger than they appear</p>
-    </AccordionList>
+      {checked ? <div>Agency</div> : <div>Budget Function</div>}
 
-    <Share
-      siteUrl={this.props.location.origin}
-      pageUrl={this.props.location.pathname + '#' + this.props.sectionId}
-      title='Data Lab - COVID-19 tracking stuff - U.S. Treasury'
-      text={'Who watches the Watchmen? Anyone with HBO...'}
-    />
+      <Share
+        siteUrl={props.location.origin}
+        pageUrl={props.location.pathname + '#' + props.sectionId}
+        title='Data Lab - COVID-19 tracking stuff - U.S. Treasury'
+        text={'Who watches the Watchmen? Anyone with HBO...'}
+      />
 
+      Look! Here's a chart...
 
-
-    <Downloads
-      href={''}
-      date={'Flovember 1922'}
-    />
-  </>;
+      <Downloads
+        href={''}
+        date={'Flovember 1922'}
+      />
+    </>
+  )
 }
+
+export default Tracking;
