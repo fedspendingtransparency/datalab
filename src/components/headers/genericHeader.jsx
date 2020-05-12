fc452887b36e0f656f5b628d04c4b815a8c40308
Brett Varney <brett.varney@kc.frb.org>
2020-05-12 11:09:24 -0500

    and change menu name

--- src/components/headers/genericHeader.jsx
+++ src/components/headers/genericHeader.jsx
@@ -11,7 +11,7 @@ export default class GenericHeader extends React.Component {
               megamenuItems={[
                 {
                   analyses: [
-                    { name: 'COVID-19', link: '/federal-covid-spending/' },
+                    { name: 'COVID-19 Spending', link: '/federal-covid-spending/' },
                     { name: 'Colleges and Universities', link: '/colleges-and-universities/' },
                     { name: 'Homelessness Analysis', link: '/homelessness-analysis/' },
                     { name: 'Federal Employees', link: '/federal-employees/' },
