d260d7f639fed64575057ba15c59822f57a83da8
Brett Varney <brett.varney@kc.frb.org>
2020-05-26 08:19:31 -0500

    minor clean up

--- src/page-sections/federal-covid-spending/tracking/index.jsx
+++ src/page-sections/federal-covid-spending/tracking/index.jsx
@@ -211,10 +211,7 @@ export default function Tracking(props) {
 	}))(Button);
 
 	const findTitle = () => {
-		let dataType = 'functions';
-		if (checked) {
-			dataType = 'agencies';
-		}
+		const dataType = checked ? 'agencies' : 'functions';
 		const selectionAmount = data[dataType].nodes.find(item => item.label === selectedBar);
 		return `${selectedBar} ${selectionAmount ? numberFormatter('dollars suffix', selectionAmount.Total_Budgetary_Resources) : ''}`;
 	}
