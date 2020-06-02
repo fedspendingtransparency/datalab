8db60fe81d3a9fd06c33bf20e7610d4f1f181f2c
anjenkin <andrea.r.jenkins@gmail.com>
2020-06-02 12:14:36 -0400

    changed font weight on modal title

--- src/page-sections/federal-covid-spending/tracking/index.jsx
+++ src/page-sections/federal-covid-spending/tracking/index.jsx
@@ -169,7 +169,7 @@ export default function Tracking(props) {
 
 	const findTitle = () => {
 		const selectionAmount = data.agencies.nodes.find(item => item.label === selectedBar);
-		return `${selectedBar} ${selectionAmount ? numberFormatter('dollars suffix', selectionAmount.Total_Budgetary_Resources) : ''}`;
+		return [<b>{selectedBar} </b>, selectionAmount ? numberFormatter('dollars suffix', selectionAmount.Total_Budgetary_Resources) : ''];
 	}
 
 	return <>
