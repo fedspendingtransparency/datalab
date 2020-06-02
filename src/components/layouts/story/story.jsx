813ad41e5a604339f72dec50c935c81de7ba1e22
Brett Varney <brett.varney@kc.frb.org>
2020-06-02 08:45:47 -0500

    reduce hero margin

--- src/components/layouts/story/story.jsx
+++ src/components/layouts/story/story.jsx
@@ -30,22 +30,21 @@ const StoryLayout = (props) => {
         </Grid>
       </Grid>;
 
-
     toc = props.sectionToc ? <Toc sections={props.sectionToc} /> : <></>;
   }
 
   return <Default>
-    <StorypageHeader/>
-      <div className={styles.storyPage}>
-        {header}
-        {toc}
-        {props.children}
+    <StorypageHeader />
+    <div className={styles.storyPage}>
+      {header}
+      {toc}
+      {props.children}
 
-        <div className={styles.hwcta}>
-          <HwctaLink url={props.hwctaLink || '#'} />
-        </div>
-        <MoreAnalyses />
+      <div className={styles.hwcta}>
+        <HwctaLink url={props.hwctaLink || '#'} />
       </div>
+      <MoreAnalyses />
+    </div>
   </Default>
 };
 
