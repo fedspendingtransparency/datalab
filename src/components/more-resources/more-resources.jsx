f7437dde54085c9f75dab67393383c7dedaa5be6
598403 <Forjan_Matthew@bah.com>
2020-06-04 13:52:01 -0400

    added MoreResources component

---
+++ src/components/more-resources/more-resources.jsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import { Link } from 'gatsby';
+import { Grid } from '@material-ui/core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDesktop, faBook } from '@fortawesome/free-solid-svg-icons';
+
+import apiImg from 'src/images/home/resources-api-2.svg';
+import userGuideImg from 'src/images/home/resources-user-guide-2.svg';
+import playbookImg from 'src/images/home/resources-playbook-2.svg';
+
+import moreResourcesStyles from './more-resources.module.scss';
+
+const MoreResources = () => {
+  const resources = [
+    {
+      href: 'https://api.usaspending.gov/',
+      imageSrc: apiImg,
+      altText: 'An icon of a computer monitor.',
+      title: 'API Guide',
+      description: 'Guidance on accessing Treasury data with open source code.'
+    }, {
+      href: 'https://www.fiscal.treasury.gov/data-transparency/DAIMS-current.html',
+      imageSrc: userGuideImg,
+      altText: 'An icon of a book.',
+      title: 'Data Model',
+      description: 'Methods to easily navigate data from our sister site, USAspending.gov.'
+    }, {
+      href: '/student-innovators-toolbox/',
+      imageSrc: playbookImg,
+      altText: 'An icon of a notebook.',
+      title: 'Student Innovators Toolbox',
+      description: 'Ways for professors and students to get involved.'
+    }
+  ];
+
+  return (
+    <section className={moreResourcesStyles.resources}>
+      <div className={moreResourcesStyles.heading}>
+        More Resources
+      </div>
+      <Grid container spacing={3} className={moreResourcesStyles.tiles}>
+        {resources.map((resource, i) => (
+          <Grid item xs={12} md={4}
+            key={'resources_tile_' + i}
+            className={`tile ${moreResourcesStyles.tile}`}
+          >
+            <a href={resource.href} target="_blank" rel="noopener noreferrer">
+              <img
+                data-src={resource.imageSrc}
+                alt={resource.altText}
+                className={`${moreResourcesStyles.svgImg} lazyload`}
+              />
+              <div className={moreResourcesStyles.text}>
+                <p className={moreResourcesStyles.title}>
+                  {resource.title}
+                </p>
+                <p className={moreResourcesStyles.subtitle}>
+                  {resource.description}
+                </p>
+              </div>
+            </a>
+          </Grid>
+        ))}
+      </Grid>
+    </section>
+  );
+}
+ 
+export default MoreResources;
