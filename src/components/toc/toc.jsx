ba302ee79214f85071f6abdee097a9ad8a1f94a3
Brett Varney <brett.varney@kc.frb.org>
2020-06-04 11:22:11 -0500

    make justify only desktop sizes

--- src/components/toc/toc.jsx
+++ src/components/toc/toc.jsx
@@ -25,7 +25,7 @@ const Toc = (props) => {
 
   return (
     <section id={styles.TOC}>
-      <Grid container justify='space-around'>
+      <Grid container className={styles.container}>
         {props.sections.map((item, key) =>
 
           <Grid item key={key} className={`${styles.tile}`} xs={12} md={6} lg={3}>
@@ -37,10 +37,10 @@ const Toc = (props) => {
                   textDecoration: 'underline'
                 }}
               />
-              <Grid container className={styles.content} justify='center'>
+              <Grid container justify='center' className={styles.content}>
                 <Grid item className={styles.a}>
                   <Grid container>
-                    <Grid item className={`${styles.number} number`} xs={2} lg={3}>
+                    <Grid item xs={2} lg={3} className={`${styles.number} number`}>
                       {item.number}
                     </Grid>
                     <Grid item className={`${styles.section} section`}>
