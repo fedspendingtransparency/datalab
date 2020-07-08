22fc1e13e325b3cce93ca6a9205d6e7245128d06
598403 <Forjan_Matthew@bah.com>
2020-07-08 12:51:18 -0400

    removed todo comments

--- src/components/headers/dropdown.jsx
+++ src/components/headers/dropdown.jsx
@@ -1,5 +1,3 @@
-//TODO: resume logical page flow when shift/tabbing beyond current menu items
-
 import React from "react";
 import PropTypes from 'prop-types';
 import styles from './page.module.scss';
