7e5f3b61dba18ea564862a8201a11ba2d4ceb628
598403 <Forjan_Matthew@bah.com>
2020-08-05 14:30:37 -0400

    fixed other specific colored components

--- src/components/section-elements/downloads/downloads.jsx
+++ src/components/section-elements/downloads/downloads.jsx
@@ -17,8 +17,8 @@ const Downloads = (props) => {
   const [screenMode, setScreenMode] = useState(0);
 
   if (typeof window !== 'undefined') {
-    if (pageColorMap[window.location.pathname]) {
-      fillColor = pageColorMap[window.location.pathname];
+    if (pageColorMap[window.location.pathname.split('/').join('')]) {
+      fillColor = pageColorMap[window.location.pathname.split('/').join('')];
     }
 
     const resizeWindow = () => {
