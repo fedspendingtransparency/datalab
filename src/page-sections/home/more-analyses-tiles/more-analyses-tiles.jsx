5dd0c7883bf9166c79bb694ac6a4938deae66740
anjenkin <andrea.r.jenkins@gmail.com>
2020-08-11 09:37:47 -0400

    fixed homelessness link

--- src/page-sections/home/more-analyses-tiles/more-analyses-tiles.jsx
+++ src/page-sections/home/more-analyses-tiles/more-analyses-tiles.jsx
@@ -18,7 +18,7 @@ const MoreAnalysesTiles = () => {
     title: 'Federal Account Explorer',
     subtitle: 'Discover the federal government\'s spending accounts'
   }, {
-    href: 'homeless',
+    href: 'homeless-analysis',
     imageSrc: homeless,
     title: 'Homelessness Analysis',
     subtitle: 'Explore federal programs that address homelessness',
