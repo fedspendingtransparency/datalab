2ba0d2bea62603623c95e9009204e89a4a167570
xjackk <xkillilea@gmail.com>
2020-09-03 11:30:22 -0400

    update image location

--- src/components/og-tag/og.jsx
+++ src/components/og-tag/og.jsx
@@ -9,17 +9,17 @@ export default class Og extends React.Component {
         super(props);
     };
 
-    render() {
-        if (typeof window !== undefined) {
-            return (
-                <Helmet>
-                    <meta property="og:image" content={origin + this.props.socialMediaImage} />
-                </Helmet>
-            )  
-        }
+  render() {
+    if (typeof window !== undefined) {
+      return (
+        <Helmet>
+          <meta property="og:image" content={origin + this.props.socialMediaImage} />
+        </Helmet>
+      );
     }
+  };
 };
 
 Og.propTypes = {
     socialMediaImage: PropTypes.string.isRequired,
-}
+};
