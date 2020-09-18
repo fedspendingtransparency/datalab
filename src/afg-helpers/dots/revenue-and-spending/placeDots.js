54352c9a46cefb1c48b769fb499bc19273e307a8
xjackk <xkillilea@gmail.com>
2020-09-17 22:01:27 -0400

    wip

--- src/afg-helpers/dots/revenue-and-spending/placeDots.js
+++ src/afg-helpers/dots/revenue-and-spending/placeDots.js
@@ -4,7 +4,6 @@ import { dotConstants, dotsPerRow } from './dotConstants';
 import { establishContainer, translator } from 'src/afg-helpers/utils';
 import { initRevenueOverlay } from './compareRevenue';
 import { initGdp } from './compareGdp';
-import { chartWidth } from './widthManager';
 import { revealCompare } from './compareManager';
 import colors from '../../../styles/afg/colors.scss';
 
@@ -15,66 +14,80 @@ let rowCount;
 let config = {};
 
 function dotFactory(container, x, y) {
-	const color = config.sectionColor;
+  const color = config.sectionColor;
 
-	container.append('circle')
-		.attr('cx', x)
-		.attr('cy', y)
-		.attr('r', 2)
-		.style('fill', color);
+  container.append('circle')
+    .attr('cx', x)
+    .attr('cy', y)
+    .attr('r', 2)
+    .style('fill', color);
 }
 
-function readyDots() {
-	if (typeof window !== 'undefined') {
-		const dotContainer = svg.append('g')
-			.classed('main-container', true)
-			.attr('transform', translator(0, 30))
-			.append('g')
-			.classed('spending-dots', true)
-			.attr('opacity', 0);
-		const oneBillion = 1000000000;
+function readyDots(width) {
+  if (typeof window !== 'undefined') {
 
-		let i = 0;
-		let dotRectHeight;
-		const sectionAmountInBillions = config.sectionAmount / oneBillion;
-		let x = dotConstants.radius;
-		let y = 2;
+    let isMobile  = (width <= 660);
 
-		for (i; i < sectionAmountInBillions; i++) {
-			dotFactory(dotContainer, x, y);
-			x += dotConstants.offset.x;
+    const dotContainer = svg.append('g')
+	  .classed('main-container', true)
+	  .attr('transform', translator(0, 30))
+	  .append('g')
+	  .classed('spending-dots', true)
+	  .attr('opacity', 0);
 
-			if ((i + 1) % dotsPerRow === 0) {
-				y += dotConstants.offset.y;
-				x = dotConstants.radius;
-			}
-		}
+    let dotVal;
+    if (isMobile) {
+      dotVal = 10000000000;
+    } else {
+      dotVal = 1000000000;
+    }
 
-		dotContainer.transition()
-			.delay(1000)
-			.duration(1000)
-			.attr('opacity', 1)
-			.on('end', revealCompare)
-			.ease();
+    let i = 0;
+    let dotRectHeight;
+    const sectionAmountInBillions = config.sectionAmount / dotVal;
+    let x = dotConstants.radius;
+    let y = 2;
 
-		dotRectHeight = d3.select('g.spending-dots')
-			.node()
-			.getBoundingClientRect().height;
+    for (i; i < sectionAmountInBillions; i++) {
+      dotFactory(dotContainer, x, y);
+      x += dotConstants.offset.x;
 
-		dotContainer.attr('data-rect-height', dotRectHeight);
+      if ((i + 1) % dotsPerRow === 0) {
+	y += dotConstants.offset.y;
+	x = dotConstants.radius;
+      }
+    }
 
-		setTimeout(() => {
-			svg.attr('height', dotRectHeight + 50);
-		}, 1000);
-	}
+    dotContainer.transition()
+      .delay(1000)
+      .duration(1000)
+      .attr('opacity', 1)
+      .on('end', revealCompare)
+      .ease();
+
+    dotRectHeight = d3.select('g.spending-dots')
+      .node()
+      .getBoundingClientRect().height;
+
+    dotContainer.attr('data-rect-height', dotRectHeight);
+
+    setTimeout(() => {
+      svg.attr('height', dotRectHeight + 50);
+    }, 1000);
+  }
 }
+
+window.addEventListener('resize', () => {
+  console.log('we are resizing');
+  //placeDots(window.innerWidth);
+});
 
 export function placeDots(_config) {
-	d3.select('.main-container').remove();
-	config = _config || config;
-	svg = establishContainer();
+  d3.select('.main-container').remove();
+  config = _config || config;
+  svg = establishContainer();
 
-	readyDots();
-	initRevenueOverlay(config);
-	initGdp(config);
+  readyDots(window.innerWidth);
+  initRevenueOverlay(config);
+  initGdp(config);
 }
