b5544e4c91267ff3760070fca8a6898530842031
anjenkin <andrea.r.jenkins@gmail.com>
2020-10-16 15:21:10 -0400

    added image and alt text

--- src/pages/americas-finance-guide/deficit/trends/index.jsx
+++ src/pages/americas-finance-guide/deficit/trends/index.jsx
@@ -64,7 +64,9 @@ function DeficitTrendsPage(props) {
 
 				<h1>Federal Deficit Trends Over Time</h1>
 
-				<img className='deficit-trend' src='/americas-finance-guide/images/deficit-trend.svg' alt='In 2000 and 2001 the federal government experienced surpluses of $237 billion and $127 billion respectively. Since 2001, the federal government has not had another surplus. While the annual deficit did not exceed $500 billion from 2002 to 2008, the annual deficit increased substantially in response to the Great Recession with annual deficits exceeding $1 trillion from 2009 to 2012. The annual deficit declined from 2012 to 2015, falling below the $500 billion mark in 2015. Since then, however, the annual deficit has grown each year to $984 billion in 2019.'></img>
+				<img className='deficit-trend'
+						 src='/americas-finance-guide/images/federal-deficit-trends-viz.svg'
+						 alt={`Since the government spent ${AfgData.current_fy_spending_short.value} and collected ${AfgData.current_fy_revenue_short.value} in ${AfgData.current_fy.value}, the deficit for the year was ${AfgData.current_fy_deficit_short.value}.`} />
 
 				<aside className='deficit-aside'>
 					<p>Since {AfgData.last_surplus.value}, the U.S. has experienced a deficit each year. Beginning in 2016, increases in spending on Social Security, health care, and interest on federal debt have outpaced the growth of federal revenue.</p>
