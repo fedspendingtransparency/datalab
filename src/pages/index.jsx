565775bc104f4f6a5d89c06d1360211213174b5a
anjenkin <andrea.r.jenkins@gmail.com>
2020-10-16 13:00:27 -0400

    changed images on the homepage

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -10,8 +10,8 @@ import ResourcesRow from 'src/page-sections/home/resources-row/resources-row';
 import SecondaryTile from 'src/page-sections/home/secondary-tile/stacked-layout/secondary-tile';
 import SEO from 'src/components/seo';
 
-import afg from 'src/images/home/CG-GIF_sm_2019.gif';
-import covid from 'src/images/home/covid.png';
+import afg from 'src/images/home/afg-feature-module.png';
+import covid from 'src/images/home/homepage-covid-image.jpg';
 import cu from 'src/images/home/collegehomepage.svg';
 import homelessImg from 'src/images/home/homelessness.png';
 import explorer from 'src/images/home/contractexplorersunburst.png';
@@ -46,13 +46,13 @@ function IndexPage() {
       >
 	<Grid item md={12} lg={8} className={homeStyles.featuredTile}>
 	  <FeatureTile
-            href="federal-covid-funding"
-            imgSrc={covid}
+            href="americas-finance-guide"
+            imgSrc={afg}
             isMain
-            imgAlt="Woman standing with medical face mask on, social distanced between two others, with a cartoon image of the U.S. Capitol in the background."
-            heading="The Federal Response to COVID-19"
-            body={`To aid the nation’s recovery from the COVID-19 pandemic, the U.S. Congress passed four special appropriations laws, which provide multiple forms of financial relief through agency spending, tax relief, and lending. As of ${CovidCopy.monthday}, approximately half of the $${CovidCopy.totalbudgetresources} trillion in available funding has been obligated. In this analysis, we break down how the funding provides financial relief, explain the process of how the money moves from legislation to the American people, and track the progress of spending.`}
-            mobileBody="Follow along as Data Lab tracks the flow of four emergency funding laws for COVID-19. As of July 1, government agencies have reported $1.3 trillion in spending."
+            imgAlt="Illustration of finance icons: dollar bill, bag of money, etc. with the text ‘Answer all your questions about federal government finance.” overlaid."
+            heading="Your Guide to America's Finances"
+            body={`Your Guide to America's Finances is an overview of U.S. government finances for 2020. Here you’ll find information on spending, revenue, the deficit, and debt. The Guide presents a series of interactive visualizations exploring each category and how it has changed over time. Ultimately, the Guide seeks to provide a comprehensive overview of the trillions of dollars collected and spent by the federal government each year.`}
+            mobileBody="Your guide to understanding the trillions of dollars that make up the federal balance sheet. Our interactive visualizations walk you through federal spending, revenue, the deficit, and debt."
 	  />
 	</Grid>
 	<Grid item md={12} lg={4}>
@@ -63,12 +63,13 @@ function IndexPage() {
 	  />
 	  <Grid item>
 	    <SecondaryTile
-              heading="Your Guide to America's Finances"
+              heading="The Federal Response to COVID-19"
               subheading="How much does the government spend and collect?"
-              href="americas-finance-guide"
-              imgSrc={afg}
-              imgAlt="The Department of Treasury building, in Washington, D.C., that includes a bronze statue of Albert Gallatin, the fourth Secretary of the Treasury."
-              body="Your guide to understanding the trillions of dollars that make up the federal balance sheet. Our interactive visualizations walk you through federal spending, revenue, the deficit, and debt."
+              href="federal-covid-funding"
+              imgSrc={covid}
+              imgAlt="Woman standing with medical face mask on, social distanced between two others, with a cartoon image of the U.S. Capitol in the background."
+              body="To aid the nation’s recovery from the COVID-19 pandemic, the U.S. Congress passed four special appropriations laws, providing several types of financial relief through agency spending, tax relief, and lending. As of July 1, approximately half of the $2.59 trillion in available funding has been obligated. In this analysis, we break down how this funding can be used, explain the process for how the money moves to the American people, and track the progress of spending."
+							mobileBody="Follow along as Data Lab tracks the flow of funding from four emergency laws for COVID-19. As of July 1, government agencies have reported $1.3 trillion in spending."
 	    />
 	  </Grid>
 	</Grid>
