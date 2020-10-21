bce06d82f6c1de2e59cce1ee69277e918f4ece23
anjenkin <andrea.r.jenkins@gmail.com>
2020-10-21 12:32:13 -0400

    new flags

--- src/page-sections/home/secondary-tile/stacked-layout/secondary-tile.jsx
+++ src/page-sections/home/secondary-tile/stacked-layout/secondary-tile.jsx
@@ -1,61 +1,85 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import secondaryAnalysesTileStyles from './secondary-tile.module.scss';
+import React from "react"
+import PropTypes from "prop-types"
+import secondaryAnalysesTileStyles from "./secondary-tile.module.scss"
 
-import { Grid, Hidden } from '@material-ui/core';
-import { Link } from 'gatsby';
+import { Grid, Hidden } from "@material-ui/core"
+import { Link } from "gatsby"
 
-const SecondaryTile = (props) => (
+const SecondaryTile = props => (
 	<section className={secondaryAnalysesTileStyles.highlight}>
 		<Link
 			to={props.href}
 			className={secondaryAnalysesTileStyles.highlightLink}
-			ga-on='click' ga-event-category='Data Lab Home Page'
-			ga-event-action={'Clicked ' + props.heading}
-		>
-			<Hidden only='md'>
+			ga-on="click"
+			ga-event-category="Data Lab Home Page"
+			ga-event-action={"Clicked " + props.heading}>
+			<Hidden only="md">
 				{/* <Grid container spacing={3}> */}
-					{/* <Grid item className={secondaryAnalysesTileStyles.headings}> */}
-					<div className={secondaryAnalysesTileStyles.headings}>
-						<p className={secondaryAnalysesTileStyles.heading}>{props.heading}</p>
-						<Hidden mdUp>
-							<p className={secondaryAnalysesTileStyles.subheading}>{props.subheading}</p>
-						</Hidden>
-					</div>
-					{/* <Grid item> */}
-						<img
-							style={{ width: '100%' }}
-							data-src={props.imgSrc}
-							className='lazyload'
-							role='presentation'
-							alt={props.imgAlt}
-						/>
-						<Hidden mdDown>
-							<p className={secondaryAnalysesTileStyles.subheading}>{props.subheading}</p>
-						</Hidden>
-					{/* </Grid> */}
-					{/* <Grid item> */}
-						<p className={secondaryAnalysesTileStyles.text}>
-							{props.body}
+				{/* <Grid item className={secondaryAnalysesTileStyles.headings}> */}
+				<div className={secondaryAnalysesTileStyles.headings}>
+					<p className={secondaryAnalysesTileStyles.heading}>{props.heading}</p>
+					<Hidden mdUp>
+						<p className={secondaryAnalysesTileStyles.subheading}>
+							{props.subheading}
 						</p>
-					{/* </Grid>
+					</Hidden>
+				</div>
+				{/* <Grid item> */}
+				<img
+					style={{
+						position: "absolute",
+						marginTop: "10px",
+						marginLeft: "-8px",
+					}}
+					src="/americas-finance-guide/images/coming-soon-flag.svg"
+					role="presentation"
+					alt=""
+				/>
+				<img
+					style={{ width: "100%" }}
+					data-src={props.imgSrc}
+					className="lazyload"
+					role="presentation"
+					alt={props.imgAlt}
+				/>
+				<Hidden mdDown>
+					<p className={secondaryAnalysesTileStyles.subheading}>
+						{props.subheading}
+					</p>
+				</Hidden>
+				{/* </Grid> */}
+				{/* <Grid item> */}
+				<p className={secondaryAnalysesTileStyles.text}>{props.body}</p>
+				{/* </Grid>
 				</Grid> */}
 			</Hidden>
 
-			<Hidden only={['xs', 'sm', 'lg', 'xl']}>
+			<Hidden only={["xs", "sm", "lg", "xl"]}>
 				<Grid container spacing={3}>
 					<Grid item md={6} className={secondaryAnalysesTileStyles.headings}>
-						<p className={secondaryAnalysesTileStyles.heading}>{props.heading}</p>
-						<p className={secondaryAnalysesTileStyles.subheading}>{props.subheading}</p>
-						<p className={secondaryAnalysesTileStyles.text}>
-							{props.body}
+						<p className={secondaryAnalysesTileStyles.heading}>
+							{props.heading}
 						</p>
+						<p className={secondaryAnalysesTileStyles.subheading}>
+							{props.subheading}
+						</p>
+						<p className={secondaryAnalysesTileStyles.text}>{props.body}</p>
 					</Grid>
 					<Grid item xs={12} md={6}>
 						<img
-							style={{ width: '100%' }}
+							style={{
+								position: "absolute",
+								marginTop: "10px",
+								marginLeft: "-8px",
+							}}
+							src="/americas-finance-guide/images/coming-soon-flag.svg"
+							role="presentation"
+							alt=""
+						/>
+						<img
+							style={{ width: "100%" }}
 							data-src={props.imgSrc}
-							className='lazyload'
+							className="lazyload"
 							alt={props.imgAlt}
 						/>
 					</Grid>
@@ -63,9 +87,9 @@ const SecondaryTile = (props) => (
 			</Hidden>
 		</Link>
 	</section>
-);
+)
 
-export default SecondaryTile;
+export default SecondaryTile
 
 SecondaryTile.propTypes = {
 	href: PropTypes.string.isRequired,
@@ -73,5 +97,5 @@ SecondaryTile.propTypes = {
 	subheading: PropTypes.string.isRequired,
 	imgSrc: PropTypes.string.isRequired,
 	imgAlt: PropTypes.string.isRequired,
-	body: PropTypes.string.isRequired
+	body: PropTypes.string.isRequired,
 }
--- src/pages/americas-finance-guide/spending/index.jsx
+++ src/pages/americas-finance-guide/spending/index.jsx
@@ -1,55 +1,64 @@
-import 'src/styles/afg/chapterIntroCommon.scss';
-import 'src/styles/afg/cg.scss';
-import 'src/page-sections/afg-spending/intro/spending-intro.scss';
+import "src/styles/afg/chapterIntroCommon.scss"
+import "src/styles/afg/cg.scss"
+import "src/page-sections/afg-spending/intro/spending-intro.scss"
 
-import React, { useEffect } from 'react';
-import SpendingIntro from 'src/page-sections/afg-spending/intro/index';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faAngleRight, faReply } from '@fortawesome/free-solid-svg-icons';
-import SEO from 'src/components/seo';
-import AfgData from '../../../../static/americas-finance-guide/_data/object_mapping.yml';
-import GdpTemplate from 'src/components/gdp-template/gdp-template';
-import AccordionList from 'src/components/accordion-list/accordion-list';
-import ControlBar from 'src/components/control-bar/control-bar';
-import Share from 'src/components/share/share';
-import Og from 'src/components/og-tag/og';
-import { setFactsTrigger } from 'src/afg-helpers/dots/revenue-and-spending/compareManager';
+import React, { useEffect } from "react"
+import SpendingIntro from "src/page-sections/afg-spending/intro/index"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faAngleRight, faReply } from "@fortawesome/free-solid-svg-icons"
+import SEO from "src/components/seo"
+import AfgData from "../../../../static/americas-finance-guide/_data/object_mapping.yml"
+import GdpTemplate from "src/components/gdp-template/gdp-template"
+import AccordionList from "src/components/accordion-list/accordion-list"
+import ControlBar from "src/components/control-bar/control-bar"
+import Share from "src/components/share/share"
+import Og from "src/components/og-tag/og"
+import { setFactsTrigger } from "src/afg-helpers/dots/revenue-and-spending/compareManager"
 
-import AnecdoteSpendingSVG from '../../../../static/americas-finance-guide/icons/anecdote-spending.svg';
-import DefinitionSpendingSVG from '../../../../static/americas-finance-guide/icons/definition.svg';
-import AfgLayout from '../../../components/layouts/afg/afg';
-
+import AnecdoteSpendingSVG from "../../../../static/americas-finance-guide/icons/anecdote-spending.svg"
+import DefinitionSpendingSVG from "../../../../static/americas-finance-guide/icons/definition.svg"
+import AfgLayout from "../../../components/layouts/afg/afg"
 
 function SpendingAndGdpPage(props) {
-  useEffect(() => {
-    setFactsTrigger();
-  }, []);
+	useEffect(() => {
+		setFactsTrigger()
+	}, [])
 
-  return (
-    <>
-      <SEO
-        title="Data Lab - Federal Spending and GDP – U.S. Treasury"
-        description="In 2019, the U.S. government spent $4.4 trillion."
+	return (
+		<>
+			<SEO
+				title="Data Lab - Federal Spending and GDP – U.S. Treasury"
+				description="In 2019, the U.S. government spent $4.4 trillion."
 				excerpt="Where does all the money go? Most government spending is concentrated on programs that ensure the health and well-being of the people of the United States. Explore how spending compares to the size of the economy or to federal revenue."
-        keywords={['federal spending, outlays, U.S. spending, U.S. revenue, gross domestic product, GDP, deficit, debt, mandatory spending, discretionary spending']}
-      />
-      <Og socialMediaImage={"/americas-finance-guide/images/social-share/social-media-share-spending.jpg"} />
-      <AfgLayout location={props.location} chapter={'spending'}>
+				keywords={[
+					"federal spending, outlays, U.S. spending, U.S. revenue, gross domestic product, GDP, deficit, debt, mandatory spending, discretionary spending",
+				]}
+			/>
+			<Og
+				socialMediaImage={
+					"/americas-finance-guide/images/social-share/social-media-share-spending.jpg"
+				}
+			/>
+			<AfgLayout location={props.location} chapter={"spending"}>
 				<div className="chapter-intro-common-wrapper spending-intro-wrapper">
 					<ControlBar>
 						<Share
 							location={props.location}
 							title="Data Lab - Federal Spending and GDP – U.S. Treasury"
 							twitter="How much money did the government spend last year? How does it compare with federal revenue and the size of the economy? Download the federal spending and GDP .CSV file from Your Guide to America’s Finances. #YourGuide #DataLab #OpenGov"
-							facebook="" reddit="" linkedin="" tumblr="" email=""
+							facebook=""
+							reddit=""
+							linkedin=""
+							tumblr=""
+							email=""
 						/>
 					</ControlBar>
+					<div className="chapter-intro__coming-soon">
+						<img src="/americas-finance-guide/images/new-data-badge.svg" />
+					</div>
 					<h1>
-						In
-						{' '}{AfgData.current_fy.value}
-						, the government spent
-						{' '}{AfgData.current_fy_spending.value}
-						.
+						In {AfgData.current_fy.value}, the government spent{" "}
+						{AfgData.current_fy_spending.value}.
 						<button className="info-box-trigger" data-box-id="per-individual">
 							<img src={AnecdoteSpendingSVG} alt="anecdote icon" />
 						</button>
@@ -57,63 +66,73 @@ function SpendingAndGdpPage(props) {
 					<div className="viz-wrapper">
 						<SpendingIntro />
 						<div className="intro-math intro-hidden">
-							<FontAwesomeIcon icon={faReply} className="fas fa-reply intro-math__icon" />
-							{AfgData.dot_number_spending.value}
-							{' '}
-							dots x
-							{' '}
-							{AfgData.dot_represents.value}
-							{' '}
-							=
-							{' '}
+							<FontAwesomeIcon
+								icon={faReply}
+								className="fas fa-reply intro-math__icon"
+							/>
+							{AfgData.dot_number_spending.value} dots x{" "}
+							{AfgData.dot_represents.value} ={" "}
 							<strong>{AfgData.current_fy_spending.value}</strong>
 						</div>
 						<div id="copy" className="intro-hidden">
-							<p>Where does all the money go? The government spends money on a variety of programs to promote the well-being of Americans. Why does the federal government do this? The Constitution provides an answer! The Constitution’s preamble states that the purpose of our government is “…to establish Justice, insure domestic Tranquility, provide for the common defense, promote the general Welfare, and secure the Blessings of Liberty to ourselves and our Posterity.”</p>
+							<p>
+								Where does all the money go? The government spends money on a
+								variety of programs to promote the well-being of Americans. Why
+								does the federal government do this? The Constitution provides
+								an answer! The Constitution’s preamble states that the purpose
+								of our government is “…to establish Justice, insure domestic
+								Tranquility, provide for the common defense, promote the general
+								Welfare, and secure the Blessings of Liberty to ourselves and
+								our Posterity.”
+							</p>
 						</div>
 						<div className="facts sidebar intro-hidden">
 							<div className="facts__inner">
 								<div id="compare-options">
-									<p className="facts__prompt">How does federal spending compare to federal revenue and the size of the economy?</p>
+									<p className="facts__prompt">
+										How does federal spending compare to federal revenue and the
+										size of the economy?
+									</p>
 									<div className="facts__triggers">
-										<button className="facts__trigger" data-trigger-id="revenue">Federal Revenue</button>
-										<button className="facts__trigger" data-trigger-id="gdp">U.S. Economy</button>
+										<button
+											className="facts__trigger"
+											data-trigger-id="revenue">
+											Federal Revenue
+										</button>
+										<button className="facts__trigger" data-trigger-id="gdp">
+											U.S. Economy
+										</button>
 									</div>
 								</div>
 								<section id="revenue-facts" className="facts__section">
 									<h1>
-										In Fiscal Year
-										{' '}{AfgData.current_fy.value}
-										, the federal government collected
-										{' '}{AfgData.current_fy_revenue.value}
-										{' '}
-										in federal revenue.
+										In Fiscal Year {AfgData.current_fy.value}, the federal
+										government collected {AfgData.current_fy_revenue.value} in
+										federal revenue.
 									</h1>
 									<p>
-										Since the government spent more than it collected, the deficit for
-										{' '}{AfgData.current_fy.value}
-										{' '}
-										was
-										{' '}{AfgData.current_fy_deficit.value}
-										.
+										Since the government spent more than it collected, the
+										deficit for {AfgData.current_fy.value} was{" "}
+										{AfgData.current_fy_deficit.value}.
 									</p>
 								</section>
 								<section id="gdp-facts" className="facts__section">
 									<h1>
-										In Fiscal Year
-										{' '}{AfgData.current_fy.value}
-										, federal spending was equal to
-										{' '}{AfgData.spending_percent_gdp.value}
-										{' '}
-										of the total gross domestic product (GDP), or economic activity, of the United States that year (
-										{AfgData.current_fy_gdp.value}
+										In Fiscal Year {AfgData.current_fy.value}, federal spending
+										was equal to {AfgData.spending_percent_gdp.value} of the
+										total gross domestic product (GDP), or economic activity, of
+										the United States that year ({AfgData.current_fy_gdp.value}
 										).
 									</h1>
 									<p>
-										Why do we compare federal spending to gross domestic product? One reason is to give a reference point for the size of the federal government, as measured by the amount it spends. U.S. gross domestic product is much larger than government spending, because it includes all the economic activity of the entire nation. Government spending equates to roughly
-										{' '}{AfgData.spending_proportion.value}
-										{' '}
-										of the goods produced and services provided in the United States.
+										Why do we compare federal spending to gross domestic
+										product? One reason is to give a reference point for the
+										size of the federal government, as measured by the amount it
+										spends. U.S. gross domestic product is much larger than
+										government spending, because it includes all the economic
+										activity of the entire nation. Government spending equates
+										to roughly {AfgData.spending_proportion.value} of the goods
+										produced and services provided in the United States.
 									</p>
 									<strong>
 										What's gross domestic product?
@@ -125,77 +144,97 @@ function SpendingAndGdpPage(props) {
 							</div>
 						</div>
 						<section className="tour sidebar intro-hidden">
-							<h1 className="tour__heading">What are the categories of federal spending?</h1>
-							<a href="/americas-finance-guide/spending/categories/" className="tour__link">
+							<h1 className="tour__heading">
+								What are the categories of federal spending?
+							</h1>
+							<a
+								href="/americas-finance-guide/spending/categories/"
+								className="tour__link">
 								Discover
-								<FontAwesomeIcon icon={faAngleRight} width={7} className="fa fa-angle-right" />
+								<FontAwesomeIcon
+									icon={faAngleRight}
+									width={7}
+									className="fa fa-angle-right"
+								/>
 							</a>
 						</section>
-					</div>
-					{' '}
+					</div>{" "}
 					{/* end viz-wrapper */}
 					<div className="info-box" id="per-individual">
 						<img src={AnecdoteSpendingSVG} alt="anecdote icon" />
 						<p>
-							How much is
-							{' '}{AfgData.current_fy_spending.value}
-							{' '}
-							in federal spending?  If you divide it by the U.S. population estimate in
-							{' '}{AfgData.current_fy.value}
-							,
-							{AfgData.us_population.value}
-							{' '}
-							(
-							<a target="_blank" href={AfgData.census_population.value} target="_blank">U.S. Census Bureau</a>
-							), it would equate to a little more than
-							{' '}{AfgData.spending_per_individual.value}
-							{' '}
-							in federal spending for every individual.
+							How much is {AfgData.current_fy_spending.value} in federal
+							spending? If you divide it by the U.S. population estimate in{" "}
+							{AfgData.current_fy.value},{AfgData.us_population.value} (
+							<a
+								target="_blank"
+								href={AfgData.census_population.value}
+								target="_blank">
+								U.S. Census Bureau
+							</a>
+							), it would equate to a little more than{" "}
+							{AfgData.spending_per_individual.value} in federal spending for
+							every individual.
 						</p>
 					</div>
 					<div className="info-box" id="gdp-info">
 						<img src={DefinitionSpendingSVG} alt="definition icon" />
 						<p>
-							<strong>Gross domestic product (GDP)</strong>
-							{' '}
-							measures the size of the nation's economy by the total value of final goods and services that are produced in a year. Gross domestic product is used to compare the economies of different countries, measure growth in the economy, and determine the right monetary policies to address inflation and unemployment.
+							<strong>Gross domestic product (GDP)</strong> measures the size of
+							the nation's economy by the total value of final goods and
+							services that are produced in a year. Gross domestic product is
+							used to compare the economies of different countries, measure
+							growth in the economy, and determine the right monetary policies
+							to address inflation and unemployment.
 						</p>
 					</div>
 					<div className="info-box" id="billion-dollars">
 						<img src={AnecdoteSpendingSVG} alt="anecdote icon" />
 						<p>
-							In this visualization, one dot represents
-							{' '}{AfgData.dot_represents.value}
-							{' '}
-							of federal spending.
-							{' '}
+							In this visualization, one dot represents{" "}
+							{AfgData.dot_represents.value} of federal spending.{" "}
 						</p>
-					</div>
-					{' '}
+					</div>{" "}
 					{/* end "ffg-wrapper spending-intro" */}
 					<section className="hwcta">
 						<AccordionList title="Data Sources and Methodology">
 							<p>
 								The visualization was created using the
-								<a href={AfgData.current_mts.value} rel="noopener noreferrer" target="_blank">Monthly Treasury Statement (MTS)</a>
-								{' '}
-								as the data source for federal government spending of the United States.
-								<GdpTemplate />
-								{' '}
-								The spending-to-gross domestic product ratio is included to provide you with context for the trillions of dollars that go out from the federal government annually. Throughout this page, we use the gross domestic product for the Fiscal Year, not the Calendar Year, in order to facilitate an appropriate comparison.
+								<a
+									href={AfgData.current_mts.value}
+									rel="noopener noreferrer"
+									target="_blank">
+									Monthly Treasury Statement (MTS)
+								</a>{" "}
+								as the data source for federal government spending of the United
+								States.
+								<GdpTemplate /> The spending-to-gross domestic product ratio is
+								included to provide you with context for the trillions of
+								dollars that go out from the federal government annually.
+								Throughout this page, we use the gross domestic product for the
+								Fiscal Year, not the Calendar Year, in order to facilitate an
+								appropriate comparison.
 							</p>
 							<div className="afg__download--div">
-								<div className="afg__download--heading">Download Source Data</div>
+								<div className="afg__download--heading">
+									Download Source Data
+								</div>
 								<ul>
-									<li><a href="/americas-finance-guide/data/federal_spending_gdp.csv" download="federal_spending_gdp.csv">federal_spending_gdp.csv</a></li>
+									<li>
+										<a
+											href="/americas-finance-guide/data/federal_spending_gdp.csv"
+											download="federal_spending_gdp.csv">
+											federal_spending_gdp.csv
+										</a>
+									</li>
 								</ul>
 							</div>
 						</AccordionList>
 					</section>
 				</div>
-      </AfgLayout>
-    </>
-  );
+			</AfgLayout>
+		</>
+	)
 }
 
-export default SpendingAndGdpPage;
+export default SpendingAndGdpPage
