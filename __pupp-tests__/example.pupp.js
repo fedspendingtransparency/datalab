133735a68c89080c0cdf3dd5974f9b03314eeef9
anjenkin <andrea.r.jenkins@gmail.com>
2020-11-05 15:34:45 -0500

    added afg and other remaining pages

--- __pupp-tests__/example.pupp.js
+++ __pupp-tests__/example.pupp.js
@@ -14,20 +14,12 @@ const pagesArray = [
 		heading: "Competition in Contracting",
 	},
 	{
-		pathname: "contract-explorer",
-		heading: "Contract Explorer",
-	},
-	{
-		pathname: "dts",
-		heading: "Visualizing the Daily Treasury Statement",
-	},
-	{
 		pathname: "federal-account-explorer",
 		heading: "Federal Account Explorer",
 	},
 	{
 		pathname: "federal-covid-funding",
-		heading: "THE FEDERAL RESPONSE TO COVID-19",
+		heading: "The Federal Response To COVID-19",
 	},
 	{
 		pathname: "federal-employees",
@@ -39,24 +31,95 @@ const pagesArray = [
 	},
 	{
 		pathname: "student-innovators-toolbox",
-		heading: "Student Innovator's Toolbox",
+		heading: "Student Innovator",
 	},
 	{
 		pathname: "404",
 		heading: "Looks like you got lost in the data",
 	},
+	{
+		pathname: "americas-finance-guide",
+		heading:
+			"How much money did the federal government collect and spend in 2020",
+	},
+	{
+		pathname: "americas-finance-guide/revenue",
+		heading:
+			"How does federal revenue compare to federal spending and the size of the economy",
+	},
+	{
+		pathname: "americas-finance-guide/revenue/categories",
+		heading: "Sources of Revenue for the Federal Government",
+	},
+	{
+		pathname: "americas-finance-guide/revenue/trends",
+		heading: "Federal Revenue Trends Over Time",
+	},
+	{
+		pathname: "americas-finance-guide/revenue/country-comparison",
+		heading: "Compare Federal Revenue of the United States to other Countries",
+	},
+	{
+		pathname: "americas-finance-guide/spending",
+		heading:
+			"How does federal spending compare to federal revenue and the size of the economy",
+	},
+	{
+		pathname: "americas-finance-guide/spending/categories",
+		heading: "Federal Spending by Category and Agency",
+	},
+	{
+		pathname: "americas-finance-guide/spending/trends",
+		heading: "Federal Spending Trends Over Time",
+	},
+	{
+		pathname: "americas-finance-guide/spending/country-comparison",
+		heading: "Compare Federal Spending of the United States to other Countries",
+	},
+	{
+		pathname: "americas-finance-guide/deficit",
+		heading:
+			"What is the deficit and how does that compare to the national debt",
+	},
+	{
+		pathname: "americas-finance-guide/deficit/trends",
+		heading: "Federal Deficit Trends Over Time",
+	},
+	{
+		pathname: "americas-finance-guide/deficit/country-comparison",
+		heading:
+			"Compare the Federal Deficit of the United States to Other Countries",
+	},
+	{
+		pathname: "americas-finance-guide/debt",
+		heading:
+			"How does the national debt compare to the deficit and the size of the economy",
+	},
+	{
+		pathname: "americas-finance-guide/debt/trends",
+		heading: "Federal Debt Trends Over Time",
+	},
+	{
+		pathname: "americas-finance-guide/debt/analysis",
+		heading: "Breakdown of the Federal Government's Debt",
+	},
+	{
+		pathname: "americas-finance-guide/debt/country-comparison",
+		heading: "Compare the Federal Debt of the United States to Other Countries",
+	},
 ]
 
 describe("Homepage Test", () => {
 	it("should return the correct response code", async () => {
+		jest.setTimeout(30000)
+
 		const pageCheck = async pageContent => {
 			const content = await page.goto(`${siteRoot}/${pageContent.pathname}`, {
 				waitUntil: "domcontentloaded",
 				timeout: 0,
 			})
 
-			await expect(page, { timeout: 0 }).toMatch(pageContent.heading)
-
+			await expect(page).toMatch(pageContent.heading)
 			expect(content._status).toBe(200)
 		}
 
@@ -64,5 +127,5 @@ describe("Homepage Test", () => {
 		for (let index = 0; index < array.length; index++) {
 			await pageCheck(array[index])
 		}
-	})
+	}, 30000)
 })
