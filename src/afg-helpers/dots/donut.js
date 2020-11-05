17452d1b538fe5368174d5d22656a3c868b2daa6
anjenkin <andrea.r.jenkins@gmail.com>
2020-11-05 09:23:19 -0500

    text fix

--- src/afg-helpers/dots/donut.js
+++ src/afg-helpers/dots/donut.js
@@ -4,115 +4,130 @@ import { translator } from 'src/afg-helpers/utils';
 import colors from '../../styles/afg/colors.scss';
 
 const d3 = {
-  select, selectAll, arc, pie,
+	select,
+	selectAll,
+	arc,
+	pie,
 };
 const arcFn = d3.arc();
-const pieFn = d3.pie()
-  .sort(null)
-  .value((d) => d);
+const pieFn = d3
+	.pie()
+	.sort(null)
+	.value(d => d);
 
 function setArcRadius(radius) {
-  arcFn.outerRadius(radius)
-    .innerRadius((radius) * 0.8);
+	arcFn.outerRadius(radius).innerRadius(radius * 0.8);
 }
 
 export function createDonut(container, percent, diameter, fillColor) {
-  const absPercent = Math.abs(percent);
-  const isNegativeVal = percent < 0;
-  const shadedColorIterator = isNegativeVal ? 1 : 0;
-  const g = container.append('g')
-    .attr('transform', translator(diameter / 2, diameter / 2));
+	const absPercent = Math.abs(percent);
+	const isNegativeVal = percent < 0;
+	const shadedColorIterator = isNegativeVal ? 1 : 0;
+	const g = container
+		.append('g')
+		.attr('transform', translator(diameter / 2, diameter / 2));
 
-  let data = [absPercent * 100, 100 - absPercent * 100];
+	let data = [absPercent * 100, 100 - absPercent * 100];
 
-  if (isNegativeVal) {
-    data = data.reverse();
-  }
+	if (isNegativeVal) {
+		data = data.reverse();
+	}
 
-  setArcRadius(diameter / 2);
+	setArcRadius(diameter / 2);
 
-  const pie = g.selectAll('.arc')
-    .data(pieFn(data))
-    .enter()
-    .append('g')
-    .attr('class', 'arc');
+	const pie = g
+		.selectAll('.arc')
+		.data(pieFn(data))
+		.enter()
+		.append('g')
+		.attr('class', 'arc');
 
-  const donut = pie.append('path')
-    .attr('d', arcFn);
+	const donut = pie.append('path').attr('d', arcFn);
 
-  donut.style('fill', (d, i) => {
-    const shadedColor = fillColor || '#dd6666';
-    return (i === shadedColorIterator) ? shadedColor : '#dddddd';
-  });
+	donut.style('fill', (d, i) => {
+		const shadedColor = fillColor || '#dd6666';
+		return i === shadedColorIterator ? shadedColor : '#dddddd';
+	});
 
-  g.append('text')
-    .text(`${Math.round(absPercent * 100)}%`)
-    .attr('fill', colors.textColorParagraph)
-    .attr('font-size', diameter / 4)
-    .attr('text-anchor', 'middle')
-    .attr('font-weight', 'bold')
-    .attr('y', diameter * 0.08);
+	g.append('text')
+		.text(`${Math.round(absPercent * 100)}%`)
+		.attr('fill', colors.textColorParagraph)
+		.attr('font-size', diameter / 4)
+		.attr('text-anchor', 'middle')
+		.attr('font-weight', 'bold')
+		.attr('y', diameter * 0.08);
 }
 
-export function createMobileDonut(container, percent, diameter, fillColor, sectionName) {
-  const absPercent = Math.abs(percent);
-  const isNegativeVal = percent < 0;
-  const shadedColorIterator = isNegativeVal ? 1 : 0;
-  const g = container.append('g')
-    .attr('transform', translator(diameter / 2, diameter / 2));
+export function createMobileDonut(
+	container,
+	percent,
+	diameter,
+	fillColor,
+	sectionName
+) {
+	const absPercent = Math.abs(percent);
+	const isNegativeVal = percent < 0;
+	const shadedColorIterator = isNegativeVal ? 1 : 0;
+	const g = container
+		.append('g')
+		.attr('transform', translator(diameter / 2, diameter / 2));
 
-  let data = [absPercent * 100, 100 - absPercent * 100];
+	let data = [absPercent * 100, 100 - absPercent * 100];
 
-  if (isNegativeVal) {
-    data = data.reverse();
-  }
+	if (isNegativeVal) {
+		data = data.reverse();
+	}
 
-  setArcRadius(diameter / 2);
+	setArcRadius(diameter / 2);
 
-  const pie = g.selectAll('.arc')
-    .data(pieFn(data))
-    .enter()
-    .append('g')
-    .attr('class', 'arc');
+	const pie = g
+		.selectAll('.arc')
+		.data(pieFn(data))
+		.enter()
+		.append('g')
+		.attr('class', 'arc');
 
-  const donut = pie.append('path')
-    .attr('d', arcFn);
+	const donut = pie.append('path').attr('d', arcFn);
 
-  donut.style('fill', (d, i) => {
-    const shadedColor = fillColor || '#dd6666';
-    return (i === shadedColorIterator) ? shadedColor : '#dddddd';
-  });
+	donut.style('fill', (d, i) => {
+		const shadedColor = fillColor || '#dd6666';
+		return i === shadedColorIterator ? shadedColor : '#dddddd';
+	});
 
-  g.append('text')
-    .text(`${Math.round(absPercent * 100)}%`)
-    .attr('fill', colors.textColorParagraph)
-    .attr('font-size', '0.875rem')
-    .attr('text-anchor', 'middle')
-    .attr('font-weight', 'bold')
-    .attr('y', diameter * 0.08);
+	g.append('text')
+		.text(`${Math.round(absPercent * 100)}%`)
+		.attr('fill', colors.textColorParagraph)
+		.attr('font-size', '0.875rem')
+		.attr('text-anchor', 'middle')
+		.attr('font-weight', 'bold')
+		.attr('y', diameter * 0.08);
 
-  const text = g.append('g')
-    .attr('transform', translator(0, diameter / 2))
-    .append('text')
-    .attr('fill', colors.textColorParagraph)
-    .attr('font-size', '0.875rem')
-    .attr('text-anchor', 'left')
-    .attr('font-weight', 'bold');
+	const text = g
+		.append('g')
+		.attr('transform', translator(0, diameter / 2))
+		.append('text')
+		.attr('fill', colors.textColorParagraph)
+		.attr('font-size', '0.875rem')
+		.attr('text-anchor', 'left')
+		.attr('font-weight', 'bold');
 
-  text.append('tspan')
-    .attr('x', diameter / 2 + 10)
-    .attr('dy', -diameter * 0.75)
-    .text(`Federal ${sectionName}`);
+	text
+		.append('tspan')
+		.attr('x', diameter / 2 + 10)
+		.attr('dy', -diameter * 0.75)
+		.text(`Federal ${sectionName}`);
 
-  text.append('tspan')
-    .attr('x', diameter / 2 + 10)
-    .attr('dy', diameter / 4 + 4)
-    .text(`accounted for ${Math.round(absPercent * 100)}%`);
+	text
+		.append('tspan')
+		.attr('x', diameter / 2 + 10)
+		.attr('dy', diameter / 4 + 4)
+		.text(`accounted for ${Math.round(absPercent * 100)}%`);
 
-  text.append('tspan')
-    .attr('x', diameter / 2 + 10)
-    .attr('dy', diameter / 4 + 4)
-    .text('of the U.S. economy');
+	text
+		.append('tspan')
+		.attr('x', diameter / 2 + 10)
+		.attr('dy', diameter / 4 + 4)
+		.text('of the U.S. economy');
 
-  d3.selectAll('.gdp-step-two').attr('opacity', 1);
+	d3.selectAll('.gdp-step-two').attr('opacity', 1);
 }
