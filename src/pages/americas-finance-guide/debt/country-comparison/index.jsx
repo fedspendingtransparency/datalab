bf889807364310edd6bf684fafa8fea33f1ac198
brianpetway <brian.petway@kc.frb.org>
2020-12-23 16:29:45 -0600

    8210 - added new yml file; made changes to the four country comparison pages copy according to changes in the MAC

--- src/pages/americas-finance-guide/debt/country-comparison/index.jsx
+++ src/pages/americas-finance-guide/debt/country-comparison/index.jsx
@@ -55,7 +55,7 @@ function DebtCountryComparisonPage(props) {
 			<SEO
 				title="Data Lab - Debt Country Comparison – U.S. Treasury"
 				description="Compare the federal debt of the United States to other countries."
-				excerpt="The U.S. government had $21.5 trillion in debt, or about 103% of U.S. gross domestic product (GDP) for 2017. Because the U.S. government has more money coming in and going out than any other country, it helps to compare the debt of the U.S. government to other countries relative to the size of their economies."
+				excerpt={`How does the federal debt of the U.S. compare to other countries? Check out Your Guide to America’s Finances for data from ${AfgData.countries_compared_debt.value}.  Check out the site’s data visualizations, then download .CSV files of the data to perform your own analysis. #YourGuide #DataLab #OpenGov" https://datalab.usaspending.gov/americas-finance-guide/debt/country-comparison/`}
 				keywords={[
 					` debt, national debt, federal debt, U.S. debt, interest rate, interest expense, total debt, cost of debt, GDP, gross domestic product, debt of US.S. compared to other countries, China, Japan, France, Germany, United Kingdom, India owners of US debt, monthly statement of the public debt, MSPD`,
 				]}
@@ -78,29 +78,19 @@ function DebtCountryComparisonPage(props) {
 					<div className="country-copy">
 						<div className="country-copy__text">
 							<p>
-								How does the United States compare to countries of similar size and
-								gross domestic product? Explore the chart, which shows the total debt of
-								the United States compared to {AfgData.countries_compared.value} other
-								countries listed in the CIA World Factbook. You can compare total debt
-								(in dollars) and debt as a percent of gross domestic product. Find a
-								country of interest and see for yourself. For instance, while the U.S.
-								federal debt was greater than that of China and Japan combined, it
-								ranked {AfgData.compare_us_debt_gdp_rank.value} in debt to gross
-								domestic product. Because the U.S. government has more money coming in
-								and going out than any other country, it helps to compare the debt of
-								the U.S. government to other countries based on the size of their
-								economies. To ensure an accurate comparison,{' '}
-								{AfgData.country_compare_year.value} debt data is used in this section,
-								not current fiscal year data.
+								How does the United States compare to countries of similar size and gross domestic product?
+								Explore the chart, which shows the total federal debt of the United States compared to total
+								government debt of {AfgData.countries_compared_debt.value} other countries.  You can compare
+								total debt (in dollars) and debt as a percent of gross domestic product. Find a country
+								of interest and see for yourself. Because the U.S. government has more money coming in
+								and going out than any other country, it helps to compare the debt of the U.S. government
+								to other countries based on the size of their economies. To ensure an accurate comparison,
+								{' '}{AfgData.country_compare_year.value} debt data is used in this section, not current fiscal year data.
 							</p>
 							<p>
 								<em>
-									Please note that the countries depicted in this chart have different
-									forms of government, and in order to gather nominal debt numbers for a
-									comprehensive list of countries, general government gross debt is used
-									for all countries except the US, for which we use only federal debt.
-									Both of these factors may impact the scope of finances reported by each
-									country.
+									Please note that the countries depicted in this chart have different forms of government,
+									and these differences may impact the scope of finances reported by each country.
 								</em>
 							</p>
 						</div>
@@ -136,45 +126,65 @@ function DebtCountryComparisonPage(props) {
 								as the data source for federal government debt of the United States.
 								Gross domestic product (GDP) figures come from the{' '}
 								<a
-									href={AfgData.imf_gdp.value}
+									href={AfgData.bea_gdp.value}
 									rel="noopener noreferrer"
 									target="_blank">
-									International Monetary Fund (IMF) World Economic Outlook Database
-									(WEOD)
+									Bureau of Economic Analysis (BEA)
 								</a>
 								. Debt figures for countries other than the United States also come from
 								the{' '}
 								<a
-									href={AfgData.imf_debt.value}
+									href={AfgData.gfs_url.value}
 									rel="noopener noreferrer"
 									target="_blank">
-									IMF WEOD
+									International Monetary Fund Global Financial Statistics
 								</a>
-								. Other countries' IMF WEOD data reflects more government debt than that
-								shown in the US's MSPD. The WEOD data can include federal, state, and
-								local debt whereas the MSPD reports only federal debt. Other countries'
-								IMF WEOD data reflects more general government debt than that shown in
-								the US's MSPD. The MSPD reports federal government debt, while the WEOD
-								data can include federal, state, and local debt. Since debt figures were
-								provided in the national currency for the selected countries, the
-								numbers were subsequently converted to U.S. dollars. Currency conversion
-								rates were pulled from{' '}
+								{' '}and{' '}
 								<a
-									href={AfgData.xe_conversion.value}
+									href={AfgData.eurostats_url.value}
 									rel="noopener noreferrer"
 									target="_blank">
-									XE.com
-								</a>{' '}
-								for {AfgData.xe_conversion_date.value}; the last day of the U.S. federal
-								government's fiscal year.
+									Eurostats
+								</a>
+								. These sources were chosen because they provide the best comparison for the following reasons:
+								<ul>
+									<li>
+										consistency with the level of government measured (central government only as a standard),
+									</li>
+									<li>
+										frequency of updates to the data,
+									</li>
+									<li>
+										use of real figures over estimates in these datasets.
+									</li>
+								</ul>
 							</p>
 							<p>
-								The conversion of debt figures to U.S. dollars makes comparisons among
-								countries more convenient. However, the implied burden of debt may be
-								misrepresented for a given country if the majority of that nation's debt
-								was denominated in a currency other than U.S. dollars, and the currency
-								in which the debt was held had an abnormal valuation relative to the
-								U.S. dollar on the date of currency conversion.
+								Countries without {AfgData.country_compare_year.value} figures were excluded from the
+								country comparison data set, as well as countries reporting only general government figures.
+								General government figures include province, state, and local debt and are not as useful
+								of a comparison to federal financial figures for the United States. Some governments rely
+								on provincial, state, or local debt than on the central government, and as a result, this
+								visualization should not be considered an absolute comparison of the debt of central
+								governments for all countries.
+							</p>
+							<p>
+								Debt figures were converted to U.S. dollars using the{' '}
+								<a
+									href={AfgData.exchange_url.value}
+									rel="noopener noreferrer"
+									target="_blank">
+									Rates of Exchange
+								</a>
+								{' '}published by the US Treasury for {AfgData.conversion_date.value}. Countries were
+								excluded that did not have both debt, and exchange rate data available.
+							</p>
+							<p>
+								The conversion of debt figures to U.S. dollars makes comparisons among countries more
+								convenient. However, the implied burden of debt may be misrepresented for a given country
+								if the majority of that nation’s debt was represented in a currency other than U.S. dollars,
+								and the currency in which the debt was held had an abnormal valuation relative to the U.S.
+								dollar on the date of currency conversion.
 							</p>
 							<div className="afg__download--div">
 								<div className="afg__download--heading">Download Source Data</div>
--- src/pages/americas-finance-guide/deficit/country-comparison/index.jsx
+++ src/pages/americas-finance-guide/deficit/country-comparison/index.jsx
@@ -56,7 +56,7 @@ function DeficitCountryComparisonPage(props) {
 			<SEO
 				title="Data Lab - Deficit Country Comparison – U.S. Treasury"
 				description="Compare the federal deficit of the United States to other countries."
-				excerpt="How does the United States compare to countries of similar size and gross domestic product (GDP)? Explore the chart below. You can compare deficits in dollars and deficit as a percent of GDP. Find a country of interest and see for yourself."
+				excerpt={`How does the U.S. deficit compare with other countries? Check out Your Guide America’s Finances for data from ${AfgData.countries_compared_deficit.value}, then download .CSV data files to perform your own analysis. #YourGuide #DataLab #OpenGov https://datalab.usaspending.gov/americas-finance-guide/deficit/country-comparison/`}
 				keywords={[
 					`Deficit, federal deficit, U.S.deficit, national deficit,  debt, national debt, federal debt, U.S. debt compared to other countries`,
 				]}
@@ -82,12 +82,11 @@ function DeficitCountryComparisonPage(props) {
 						<div className="country-copy__text">
 							<p>
 								How does the United States compare to countries of similar size and
-								gross domestic product? Explore the chart, which shows the total revenue
-								of the United States compared to {AfgData.countries_compared.value}{' '}
-								other countries listed in the CIA World Factbook. You can compare
-								deficits (in dollars) and deficit as a percent of gross domestic
+								gross domestic product? Explore the chart, which shows the total deficit
+								of the United States compared to {AfgData.countries_compared_deficit.value}{' '}
+								other countries. You can compare deficits (in dollars) and deficit as a percent of gross domestic
 								product. Find a country of interest and see for yourself. To ensure an
-								accurate comparison, {AfgData.country_compare_year.value} revenue data
+								accurate comparison, {AfgData.country_compare_year.value} deficit data
 								is used in this section, not current fiscal year data.{' '}
 							</p>
 							<p>
@@ -136,52 +135,50 @@ function DeficitCountryComparisonPage(props) {
 									target="_blank">
 									Bureau of Economic Analysis (BEA)
 								</a>
-								. GDP data for countries other than the United States comes from the{' '}
-								<a
-									href={AfgData.imf_gdp.value}
-									rel="noopener noreferrer"
-									target="_blank">
-									International Monetary Fund (IMF) World Economic Outlook Database
-									(WEOD)
-								</a>
 								.
 							</p>
 							<p>
-								In researching potential data sources for information on the revenue and
-								spending of other governments for the country comparison module, we
-								chose the{' '}
+								In researching potential data sources for information on the revenue and spending and deficit as a
+								percent of GDP of other governments for the country comparison module, the{' '}
 								<a
-									href={AfgData.cia_world_factbook.value}
+									href={AfgData.gfs_url.value}
 									rel="noopener noreferrer"
 									target="_blank">
-									CIA World Factbook
-								</a>{' '}
-								because it provides the best comparison for the following reasons:
+									International Monetary Fund Global Financial Statistics
+								</a>
+								{' '}and{' '}
+								<a
+									href={AfgData.eurostats_url.value}
+									rel="noopener noreferrer"
+									target="_blank">
+									Eurostats
+								</a>
+								{' '}were chosen because they provide the best comparison for the following reasons:
 							</p>
 							<ul>
 								<li>
-									the number of countries with {AfgData.country_compare_year.value} data,
+									consistency with the level of government measured (central government only as a standard),
 								</li>
 								<li>
-									relative consistency with the level of government measured (central
-									government only as a standard),
+									frequency of updates to the data,
 								</li>
-								<li>all figures expressed in US dollars.</li>
+								<li>
+									use of real figures over estimates in these datasets.
+								</li>
 							</ul>
 							<p>
-								Countries with figures before {AfgData.country_compare_year.value} were
-								excluded from the country comparison data set. Although most countries
-								in the data set feature revenue and spending from central government
-								sources only, some countries included state/provincial/local data. As a
-								result, this visualization should not be considered an absolute
-								comparison of the revenue and spending of central governments for all
-								countries.
+								Countries without {AfgData.country_compare_year.value} figures were excluded from the country comparison data set, as well as countries reporting only general government figures.  General government figures include province, state, and local revenue and are not as useful of a comparison to federal financial figures for the United States. Some governments rely more heavily on provincial, state, or local revenue than on the central government, and as a result, this visualization should not be considered an absolute comparison of the revenue of central governments for all countries.
 							</p>
 							<p>
-								To finish the data set for Country Comparison, gross domestic product
-								figures from BEA and the IMF WEOD were combined with revenue and
-								spending figures from the CIA World Factbook data set, excluding
-								countries that did not have data available from both sources.
+								Deficit figures were converted to U.S. dollars using the{' '}
+								<a
+									href={AfgData.exchange_url.value}
+									rel="noopener noreferrer"
+									target="_blank">
+									Rates of Exchange
+								</a>
+								{' '}published by the US Treasury for {AfgData.conversion_date.value}.
+								Countries were excluded that did not have revenue, spending, and exchange rate data available.
 							</p>
 							<div className="afg__download--div">
 								<div className="afg__download--heading">Download Source Data</div>
