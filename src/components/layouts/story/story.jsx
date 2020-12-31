97dac0189f75c91cbef5b4ebda5ca4133814be15
anjenkin <andrea.r.jenkins@gmail.com>
2020-12-30 21:35:06 -0500

    switch mui imports

--- src/components/layouts/story/story.jsx
+++ src/components/layouts/story/story.jsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 import Default from '../default/default';
-import { Grid } from '@material-ui/core';
+import Grid from '@material-ui/core/Grid';
 import HWCTALink from '../../hwcta-link/hwcta-link';
 import MoreAnalyses from '../../more-analyses/more-analyses';
 import { StorypageHeader } from '../../headers/headers';
@@ -11,63 +11,56 @@ import Toc from '../../toc/toc';
 import pageColorMap from 'src/utils/page-color';
 import { legacy } from '../../../styles/variables.scss';
 
-const StoryLayout = (props) => {
-  let header, toc;
+const StoryLayout = props => {
+	let header, toc;
 
-  if (!props.isCustomHeader) {
-    header =
-      <Grid container justify='center' className={styles.headerContainer}>
-        <Grid item md={10}>
-          <header className={styles.headerHero}>
-            <p className={styles.title}>
-              {props.title}
-            </p>
-            <p className={styles.introSentence}>
-              {props.introSentence}
-            </p>
-            <p className={styles.contextStatement}>
-              {props.contextStatement}
-            </p>
-          </header>
-        </Grid>
-      </Grid>;
+	if (!props.isCustomHeader) {
+		header = (
+			<Grid container justify="center" className={styles.headerContainer}>
+				<Grid item md={10}>
+					<header className={styles.headerHero}>
+						<p className={styles.title}>{props.title}</p>
+						<p className={styles.introSentence}>{props.introSentence}</p>
+						<p className={styles.contextStatement}>{props.contextStatement}</p>
+					</header>
+				</Grid>
+			</Grid>
+		);
 
-    toc = props.sectionToc ? <Toc sections={props.sectionToc} /> : <></>;
-  }
+		toc = props.sectionToc ? <Toc sections={props.sectionToc} /> : <></>;
+	}
 
-  const [color, setColor] = useState(legacy);
+	const [color, setColor] = useState(legacy);
 
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      setColor(pageColorMap[window.location.pathname.split('/').join('')])
-    }
-  }, [props.color])
+	useEffect(() => {
+		if (typeof window !== 'undefined') {
+			setColor(pageColorMap[window.location.pathname.split('/').join('')]);
+		}
+	}, [props.color]);
 
-  return <Default>
-    <StorypageHeader />
-    <div className={styles.storyPage}>
-      {header}
-      {toc}
-      {props.children}
+	return (
+		<Default>
+			<div className={styles.storyPage}>
+				{header}
+				{toc}
+				{props.children}
 
-      <div className={styles.hwcta}>
-        <HWCTALink
-          url={props.hwctaLink || '#'}
-          fillColor={color}
-        />
-      </div>
-      <MoreAnalyses />
-    </div>
-  </Default>
+				<div className={styles.hwcta}>
+					<HWCTALink url={props.hwctaLink || '#'} fillColor={color} />
+				</div>
+				<MoreAnalyses />
+			</div>
+		</Default>
+	);
 };
 
 export default StoryLayout;
 
 StoryLayout.propTypes = {
-  children: PropTypes.node.isRequired,
-  title: PropTypes.string.isRequired,
-  introSentence: PropTypes.string.isRequired,
-  hwctaLink: PropTypes.string.isRequired,
-  contextStatement: PropTypes.string,
-  toc: PropTypes.array
-}
+	children: PropTypes.node.isRequired,
+	title: PropTypes.string.isRequired,
+	introSentence: PropTypes.string.isRequired,
+	hwctaLink: PropTypes.string.isRequired,
+	contextStatement: PropTypes.string,
+	toc: PropTypes.array,
+};
--- src/components/section-elements/downloads/downloads.jsx
+++ src/components/section-elements/downloads/downloads.jsx
@@ -1,128 +1,144 @@
 import React, { useState, useEffect } from 'react';
-import downloadsStyles from "./downloads.module.scss";
+import downloadsStyles from './downloads.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faDownload } from "@fortawesome/free-solid-svg-icons";
-import { Grid, Container } from "@material-ui/core";
-import { withStyles } from "@material-ui/styles"
-import PropTypes from "prop-types";
+import { faDownload } from '@fortawesome/free-solid-svg-icons';
+import Grid from '@material-ui/core/Grid';
+import Container from '@material-ui/core/Container';
+import withStyles from '@material-ui/styles/withStyles';
+import PropTypes from 'prop-types';
 import styleVariables from 'src/styles/variables.scss';
 import FiscalDataLogo from 'src/svgs/powered-by-fiscal-data.svg';
 import pageColorMap from '../../../utils/page-color';
-import { checkScreenMode, ScreenModeEnum } from '../../../utils/screen-mode'
+import { checkScreenMode, ScreenModeEnum } from '../../../utils/screen-mode';
 
-const Downloads = (props) => {
-  let fillColor = styleVariables.legacy;
+const Downloads = props => {
+	let fillColor = styleVariables.legacy;
 
-  const [screenMode, setScreenMode] = useState(0);
+	const [screenMode, setScreenMode] = useState(0);
 
-  if (typeof window !== 'undefined') {
-    if (pageColorMap[window.location.pathname.split('/').join('')]) {
-      fillColor = pageColorMap[window.location.pathname.split('/').join('')];
-    }
+	if (typeof window !== 'undefined') {
+		if (pageColorMap[window.location.pathname.split('/').join('')]) {
+			fillColor = pageColorMap[window.location.pathname.split('/').join('')];
+		}
 
-    const resizeWindow = () => {
-      const newMode = checkScreenMode(window.innerWidth);
-      if (newMode !== screenMode) {
-        setScreenMode(newMode);
-      }
-    }
+		const resizeWindow = () => {
+			const newMode = checkScreenMode(window.innerWidth);
+			if (newMode !== screenMode) {
+				setScreenMode(newMode);
+			}
+		};
 
-    useEffect(() => {
-      resizeWindow();
-      window.addEventListener('resize', resizeWindow);
-      return () => {
-        window.removeEventListener('resize', resizeWindow);
-      }
-    }, []);
-  }
+		useEffect(() => {
+			resizeWindow();
+			window.addEventListener('resize', resizeWindow);
+			return () => {
+				window.removeEventListener('resize', resizeWindow);
+			};
+		}, []);
+	}
 
-  const DownloadsContainer = withStyles(() => ({
-    'root': {
-      padding: 0,
-      margin: 0,
-      width: 'max-content',
-      '& *': {
-        color: '#555'
-      },
-      '&:hover': {
-        '& *': {
-          color: fillColor
-        },
-        '& div': {
-          textDecoration: 'underline'
-        }
-      },
-      '& a': {
-        '&:focus': {
-          '& *': {
-            color: fillColor
-          },
-          '& div': {
-            textDecoration: 'underline'
-          }
-        }
-      }
-    }
-  }))(Container)
+	const DownloadsContainer = withStyles(() => ({
+		root: {
+			padding: 0,
+			margin: 0,
+			width: 'max-content',
+			'& *': {
+				color: '#555',
+			},
+			'&:hover': {
+				'& *': {
+					color: fillColor,
+				},
+				'& div': {
+					textDecoration: 'underline',
+				},
+			},
+			'& a': {
+				'&:focus': {
+					'& *': {
+						color: fillColor,
+					},
+					'& div': {
+						textDecoration: 'underline',
+					},
+				},
+			},
+		},
+	}))(Container);
 
-  function exportToJsonFile(jsonData) {
-    if (typeof Blob === 'undefined') {
-      return <></>;
-    }
+	function exportToJsonFile(jsonData) {
+		if (typeof Blob === 'undefined') {
+			return <></>;
+		}
 
-    const dataStr = JSON.stringify(jsonData);
-    const dataBlob = new Blob([dataStr], { type: "application/json" });
-    const dataUri = URL.createObjectURL(dataBlob);
-    const exportFileDefaultName = 'data.json';
+		const dataStr = JSON.stringify(jsonData);
+		const dataBlob = new Blob([dataStr], { type: 'application/json' });
+		const dataUri = URL.createObjectURL(dataBlob);
+		const exportFileDefaultName = 'data.json';
 
-    return (
-      <DownloadsContainer data-testid='downloads-container'>
-        <a className={downloadsStyles.data} href={dataUri} download={exportFileDefaultName}>
-          <FontAwesomeIcon icon={faDownload} width={16} />
-          <div data-testid='downloads-container-text'>&nbsp;Download</div>
-        </a>
-      </DownloadsContainer>
-    );
-  }
+		return (
+			<DownloadsContainer data-testid="downloads-container">
+				<a
+					className={downloadsStyles.data}
+					href={dataUri}
+					download={exportFileDefaultName}>
+					<FontAwesomeIcon icon={faDownload} width={16} />
+					<div data-testid="downloads-container-text">&nbsp;Download</div>
+				</a>
+			</DownloadsContainer>
+		);
+	}
 
-  const align = screenMode === ScreenModeEnum.mobile || screenMode === ScreenModeEnum.tablet ? 'center' : 'flex-start';
+	const align =
+		screenMode === ScreenModeEnum.mobile || screenMode === ScreenModeEnum.tablet
+			? 'center'
+			: 'flex-start';
 
-  return (
-    <Grid
-      container
-      alignItems={align}
-      justify={props.justify || "flex-end"}
-      direction="row"
-      className={downloadsStyles.download}
-      id={props.mobileSpace ? downloadsStyles.downloadMobile : ``}
-    >
-      {props.withFiscalDataLogo &&
-        <a target="_blank" rel="noopener noreferrer" href="https://fiscaldata.treasury.gov/">
-          <FiscalDataLogo data-testid='fiscal-data-logo' className={downloadsStyles.logo} />
-        </a>
-      }
-      {props.date ? <span data-testid='updated-date' className={downloadsStyles.fadedModifier}>Updated as of {props.date} / </span> : ''}
-      {props.isJSON ?
-        exportToJsonFile(props.data)
-        :
-        <DownloadsContainer data-testid='downloads-container'>
-          <a className={downloadsStyles.data} href={props.href}>
-              <FontAwesomeIcon icon={faDownload} width={16} />
-              <div data-testid='downloads-container-text'>&nbsp;Download</div>
-          </a>
-        </DownloadsContainer>
-      }
-    </Grid>
-  );
+	return (
+		<Grid
+			container
+			alignItems={align}
+			justify={props.justify || 'flex-end'}
+			direction="row"
+			className={downloadsStyles.download}
+			id={props.mobileSpace ? downloadsStyles.downloadMobile : ``}>
+			{props.withFiscalDataLogo && (
+				<a
+					target="_blank"
+					rel="noopener noreferrer"
+					href="https://fiscaldata.treasury.gov/">
+					<FiscalDataLogo
+						data-testid="fiscal-data-logo"
+						className={downloadsStyles.logo}
+					/>
+				</a>
+			)}
+			{props.date ? (
+				<span data-testid="updated-date" className={downloadsStyles.fadedModifier}>
+					Updated as of {props.date} /{' '}
+				</span>
+			) : (
+				''
+			)}
+			{props.isJSON ? (
+				exportToJsonFile(props.data)
+			) : (
+				<DownloadsContainer data-testid="downloads-container">
+					<a className={downloadsStyles.data} href={props.href}>
+						<FontAwesomeIcon icon={faDownload} width={16} />
+						<div data-testid="downloads-container-text">&nbsp;Download</div>
+					</a>
+				</DownloadsContainer>
+			)}
+		</Grid>
+	);
 };
 
 Downloads.propTypes = {
-  data: PropTypes.arrayOf(PropTypes.object),
-  date: PropTypes.string,
-  href: PropTypes.string,
-  mobileSpace: PropTypes.bool
+	data: PropTypes.arrayOf(PropTypes.object),
+	date: PropTypes.string,
+	href: PropTypes.string,
+	mobileSpace: PropTypes.bool,
 };
 
 export default Downloads;
-
-
