8aa2d6e3b3c37eaf59aa7216943e472ff1162df2
anjenkin <andrea.r.jenkins@gmail.com>
2021-01-13 13:02:49 -0500

    fixed height of homelessness section 2 accordion

--- src/components/accordion-list/accordion-list.jsx
+++ src/components/accordion-list/accordion-list.jsx
@@ -8,26 +8,27 @@ import accordionStyles from './accordion-list.module.scss';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus, faMinus } from '@fortawesome/free-solid-svg-icons';
+import Grid from '@material-ui/core/Grid';
 
 export default class AccordionList extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			closed: true
+			closed: true,
 		};
 
 		this.toggle = this.toggle.bind(this);
 	}
 
-  /* props notes
+	/* props notes
     title: shows in the top box, collapsed or open
     color: theme color for border, collapsed background, and highlighted text within
     backgroundColor: background of collapsed box
   */
 	static propTypes = {
-		'title': PropTypes.string.isRequired,
-		'color': PropTypes.string,
-		'backgroundColor': PropTypes.string
+		title: PropTypes.string.isRequired,
+		color: PropTypes.string,
+		backgroundColor: PropTypes.string,
 	};
 
 	toggle(e) {
@@ -48,30 +49,46 @@ export default class AccordionList extends React.Component {
 
 	render() {
 		return (
-			<div className={this.props.containerClass ? this.props.containerClass : accordionStyles.container}>
-				<div className='row'>
-					<div className='col-xs-12'>
+			<div
+				className={
+					this.props.containerClass
+						? this.props.containerClass
+						: accordionStyles.container
+				}>
+				<Grid container>
+					<Grid item xs={12}>
 						<section
-							className={!this.state.closed ? `${accordionStyles.accordion} ${accordionStyles.open} accordion--open` : `${accordionStyles.accordion}`}
-							style={this.props.color ? { 'borderColor': this.props.color } : {}}
-						>
+							className={
+								!this.state.closed
+									? `${accordionStyles.accordion} ${accordionStyles.open} accordion--open`
+									: `${accordionStyles.accordion}`
+							}
+							style={this.props.color ? { borderColor: this.props.color } : {}}>
 							<h1
 								onClick={this.toggle}
 								className={accordionStyles.heading}
-								style={this.styleOverrides()}
-							>
+								style={this.styleOverrides()}>
 								{this.props.title}
-								<button onClick={this.toggle} className={accordionStyles.toggle} aria-label={this.state.closed ? 'Open toggle' : 'Close toggle'}>
-									<FontAwesomeIcon icon={faPlus} className={accordionStyles.plus} style={this.props.color ? { 'color': this.props.color } : {}} />
-									<FontAwesomeIcon icon={faMinus} className={accordionStyles.minus} style={this.props.color ? { 'color': this.props.color } : {}} />
+								<button
+									onClick={this.toggle}
+									className={accordionStyles.toggle}
+									aria-label={this.state.closed ? 'Open toggle' : 'Close toggle'}>
+									<FontAwesomeIcon
+										icon={faPlus}
+										className={accordionStyles.plus}
+										style={this.props.color ? { color: this.props.color } : {}}
+									/>
+									<FontAwesomeIcon
+										icon={faMinus}
+										className={accordionStyles.minus}
+										style={this.props.color ? { color: this.props.color } : {}}
+									/>
 								</button>
 							</h1>
-							<div className={accordionStyles.content}>
-								{this.props.children}
-							</div>
+							<div className={accordionStyles.content}>{this.props.children}</div>
 						</section>
-					</div>
-				</div>
+					</Grid>
+				</Grid>
 			</div>
 		);
 	}
