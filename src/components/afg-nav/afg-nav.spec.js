e5a0874b4222233550f3c1110f7520211d05b3cd
brianpetway <brian.petway@kc.frb.org>
2021-01-20 13:03:21 -0600

    8466 - merged dev into forjan's old branch; added a few tests for desktop size

--- src/components/afg-nav/afg-nav.spec.js
+++ src/components/afg-nav/afg-nav.spec.js
@@ -3,7 +3,7 @@ import { render } from '@testing-library/react';
 import AfgNav from './afg-nav';
 
 describe('AfgNav', () => {
-  it('expect the 5 chevrons to be rendered', () => {
+  it('expects the 5 chevrons to be rendered', () => {
     const { getByText } = render(<AfgNav />);
 
     expect(getByText('Overview')).toBeTruthy();
@@ -13,24 +13,27 @@ describe('AfgNav', () => {
     expect(getByText('Debt')).toBeTruthy();
   });
 
-  it('expect there to be an extension on the overview section with no active section', () => {
+  it('expects there to be an extension on the overview section with no active section', () => {
     const { getByTestId, queryByTestId } = render(<AfgNav />);
     expect(getByTestId('overview-extension')).toBeTruthy();
     expect(queryByTestId('revenue-extension')).toBeNull();
   });
 
-  it('expect there to be an extension on the active section', () => {
+  it('expects there to be an extension on the active section', () => {
     const { getByTestId, queryByTestId } = render(<AfgNav chapter="revenue" />);
     expect(getByTestId('revenue-extension')).toBeTruthy();
     expect(queryByTestId('overview-extension')).toBeNull();
   });
 
-  it('expect all sections to be closed on the overview page', () => {
+  it('expects all sections to be closed on the overview page', () => {
     const { getByTestId } = render(<AfgNav />);
     expect(getByTestId('revenue-subpages')).toHaveStyle({ width: 0 });
+    expect(getByTestId('spending-subpages')).toHaveStyle({ width: 0 });
+    expect(getByTestId('deficit-subpages')).toHaveStyle({ width: 0 });
+    expect(getByTestId('debt-subpages')).toHaveStyle({ width: 0 });
   });
 
-  it('expect only the active section to be open on a specific page', () => {
+  it('expects only the active section to be open on a specific page', () => {
     const { getByTestId } = render(<AfgNav chapter="revenue" />);
     expect(getByTestId('revenue-subpages')).toHaveStyle({ width: '551px' }); // width specified when sections are created
     expect(getByTestId('spending-subpages')).toHaveStyle({ width: 0 });
