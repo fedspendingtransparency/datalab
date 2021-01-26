2555d39f1c61ee21f8dc96bdce5fc7e7746d8eef
anjenkin <andrea.r.jenkins@gmail.com>
2021-01-26 14:29:18 -0500

    wip

--- src/components/scrolling-circles/scrolling-circles.jsx
+++ src/components/scrolling-circles/scrolling-circles.jsx
@@ -7,155 +7,174 @@ import { checkScreenMode, ScreenModeEnum } from '../../utils/screen-mode';
 
 const ScrollingCircles = ({ sections }) => {
 	const [screenMode, setScreenMode] = useState(0);
-  const [activeSection, setActiveSection] = useState(sections[0].anchor);
-  const [fillColor, setFillColor] = useState(legacy);
-  const [topMargin, setTopMargin] = useState(106);
-  const [fadeClass, setFadeClass] = useState('');
+	const [activeSection, setActiveSection] = useState(sections[0].anchor);
+	const [fillColor, setFillColor] = useState(legacy);
+	const [topMargin, setTopMargin] = useState(106);
+	const [fadeClass, setFadeClass] = useState('');
 
-  const [positions, setPositions] = useState([]);
+	const [positions, setPositions] = useState([]);
+	const [currentY, setCurrentY] = useState({
+		budget: null,
+		overview: null,
+		tracking: null,
+	});
 
-  const fade = () => {
-    setFadeClass('');
-    setTimeout(() => {
-      setFadeClass(styles.fade);
-    }, 2000);
-  };
+	const fade = () => {
+		setFadeClass('');
+		setTimeout(() => {
+			setFadeClass(styles.fade);
+		}, 2000);
+	};
 
-  useEffect(() => {
-    window.scrollTo(0, 0);
-    const pathname = window.location.pathname.split('/').join('');
-    setFillColor(pageColorMap[pathname]);
+	const options = {
+		root: null,
+		rootMargin: '0px',
+		threshold: [...Array(100).keys()].map(x => x / 100),
+	};
 
-    const sectionScrollPositions = [];
-    const observe = () => {
-      setPositions([]);
+	const observer = new IntersectionObserver(entries => {
+		entries.forEach(entry => {
+			const ratio = entry.intersectionRatio;
+			const boundingRect = entry.boundingClientRect;
+			const intersectionRect = entry.intersectionRect;
+			const currentY = entry.boundingClientRect.y;
+			const section = entry.target.id.replace('section-', '');
 
-      sections.forEach((section) => {
-        const target = document.getElementById(`section-${section.anchor}`);
+			if (ratio === 0) {
+				console.log('outside');
+			} else if (ratio < 1) {
+				console.log(boundingRect.bottom);
+				console.log(boundingRect.top);
 
-        const observer = new IntersectionObserver((entries) => {
-          entries.forEach((entry) => {
-            const id = entry.target.id.split('-')[1];
-            const { top, bottom } = entry.boundingClientRect;
-            
-            if (!sectionScrollPositions.find((s) => s.id === id)) {
-              sectionScrollPositions.push({
-                id,
-                top,
-                bottom,
-              });
-            }
+				if (boundingRect.top > 50 && boundingRect.top < 100) {
+					console.log('top in scroll view');
+					// if scroll direction is down then set the section state and show the flag
+				} else if (boundingRect.bottom > 50 && boundingRect.bottom < 100) {
+					console.log('bottom in view');
+					// if scroll direction is up then set the section state and show the flag
+				} else if (boundingRect.top >= 100 || boundingRect.bottom <= 50) {
+					console.log('on page but not inview');
+				} else {
+					console.log('inview');
+				}
+			}
+			setCurrentY(prevState => ...prevState.currentY, [section]: boundingRect.y)
+		});
+	}, options);
 
-            setPositions(sectionScrollPositions);
-          });
-        });
+	useEffect(() => {
+		window.scrollTo(0, 0);
+		const pathname = window.location.pathname.split('/').join('');
+		setFillColor(pageColorMap[pathname]);
 
-        observer.observe(target);
-      });
-    };
+		const sectionScrollPositions = [];
 
-    setTimeout(observe, 2000);
+		sections.forEach(section => {
+			const target = document.getElementById(`section-${section.anchor}`);
+			observer.observe(target);
+			// setPositions(sectionScrollPositions);
+		});
 
-    window.addEventListener('scroll', () => {
-      const scrollPositions = sectionScrollPositions.sort((a, b) => a.bottom - b.bottom);
-      const newActiveSection = scrollPositions.find((s) => s.bottom > window.pageYOffset);
+		// window.addEventListener('scroll', () => {
+		//   const scrollPositions = sectionScrollPositions.sort((a, b) => a.bottom - b.bottom);
+		//   const newActiveSection = scrollPositions.find((s) => s.bottom > window.pageYOffset);
+		//
+		//   if (newActiveSection) {
+		//     setActiveSection(newActiveSection.id);
+		//   }
+		//
+		//   if (window.pageYOffset <= 26) {
+		//     setTopMargin(106 - window.pageYOffset);
+		//   } else {
+		//     setTopMargin(80);
+		//   }
+		// });
 
-      if (newActiveSection) {
-        setActiveSection(newActiveSection.id);
-      }
+		// const resizeWindow = () => {
+		// 	const newMode = checkScreenMode(window.innerWidth);
+		// 	if (newMode !== screenMode) {
+		// 		setScreenMode(newMode);
+		// 	}
+		// };
+		//
+		// resizeWindow();
+		//
+		// window.addEventListener('resize', () => {
+		// 	resizeWindow();
+		// 	observe();
+		// });
+	});
 
-      if (window.pageYOffset <= 26) {
-        setTopMargin(106 - window.pageYOffset);
-      } else {
-        setTopMargin(80);
-      }
-    });
+	useEffect(() => {
+		fade();
+	}, [activeSection]);
 
-    const resizeWindow = () => {
-      const newMode = checkScreenMode(window.innerWidth);
-      if (newMode !== screenMode) {
-        setScreenMode(newMode);
-      }
-    };
+	const scrollToSection = (id, e) => {
+		console.log('hello');
+		if (!e || e.key === 'Enter') {
+			fade();
+		}
+	};
 
-    resizeWindow();
+	const handleFocus = e => {
+		e.target.classList.add(styles.focused);
+	};
 
-    window.addEventListener('resize', () => {
-      resizeWindow();
-      observe();
-    });
-  }, []);
+	const handleBlur = e => {
+		e.target.classList.remove(styles.focused);
+	};
 
-  useEffect(() => {
-    fade();
-  }, [activeSection]);
+	return (
+		<div className={styles.mainContainer} style={{ top: topMargin }}>
+			{sections.map((section, number) => {
+				const isActive = activeSection === section.anchor;
 
-  const scrollToSection = (id, e) => {
-    if (!e || e.key === 'Enter') {
-      const section = positions.find((s) => s.id === id);
-      let scrollMargin = 25;
-      if (screenMode === ScreenModeEnum.tablet) {
-        scrollMargin = 10;
-      }
-      window.scrollTo(0, section.top + scrollMargin);
-      fade();
-    }
-  };
+				let label = <></>;
+				let activeStyle = {
+					color: fillColor,
+				};
 
-  const handleFocus = (e) => {
-    e.target.classList.add(styles.focused);
-  };
+				if (isActive) {
+					activeStyle = {
+						backgroundColor: fillColor,
+					};
 
-  const handleBlur = (e) => {
-    e.target.classList.remove(styles.focused);
-  };
+					label = (
+						<div className={`${styles.label} ${fadeClass}`} style={activeStyle}>
+							<div
+								className={styles.beforeArrow}
+								style={{ borderRight: `solid 10px ${fillColor}` }}
+							/>
+							{section.section}
+						</div>
+					);
+				}
 
-  return (
-    <div className={styles.mainContainer} style={{ top: topMargin }}>
-      {sections.map((section, number) => {
-        const isActive = activeSection === section.anchor;
+				if (!section.comingSoon && sections.length > 1) {
+					return (
+						<div className={styles.sectionContainer}>
+							<a
+								href={`#section-${section.anchor}`}
+								onClick={() => scrollToSection(section.anchor)}
+								onKeyPress={e => scrollToSection(section.anchor, e)}>
+								<div
+									className={`${styles.circle} ${isActive ? styles.active : ''}`}
+									style={activeStyle}
+									tabIndex={0}
+									onFocus={handleFocus}
+									onBlur={handleBlur}>
+									{section.number || `0${number + 1}`}
+								</div>
+							</a>
+							{label}
+						</div>
+					);
+				}
 
-        let label = <></>;
-        let activeStyle = {
-          color: fillColor,
-        };
-
-        if (isActive) {
-          activeStyle = {
-            backgroundColor: fillColor,
-          };
-
-          label = (
-            <div className={`${styles.label} ${fadeClass}`} style={activeStyle}>
-              <div className={styles.beforeArrow} style={{ borderRight: `solid 10px ${fillColor}` }} />
-              {section.section}
-            </div>
-          );
-        }
-
-        if (!section.comingSoon && sections.length > 1) {
-          return (
-            <div className={styles.sectionContainer}>
-              <div
-                className={`${styles.circle} ${isActive ? styles.active : ''}`}
-                style={activeStyle}
-                tabIndex={0}
-                onClick={() => scrollToSection(section.anchor)}
-                onKeyPress={(e) => scrollToSection(section.anchor, e)}
-                onFocus={handleFocus}
-                onBlur={handleBlur}
-              >
-                {section.number || `0${number + 1}`}
-              </div>
-              {label}
-            </div>
-          );
-        }
-
-        return null;
-      })}
-    </div>
-  );
+				return null;
+			})}
+		</div>
+	);
 };
 
 export default ScrollingCircles;
