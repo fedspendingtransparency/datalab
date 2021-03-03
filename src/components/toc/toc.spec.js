f74f93759654c6bc44c15f556958784dd00c734a
brianpetway <brian.petway@kc.frb.org>
2021-03-03 12:17:23 -0600

    8538 - wip; added some tests

--- src/components/toc/toc.spec.js
+++ src/components/toc/toc.spec.js
@@ -9,21 +9,22 @@ describe('Toc', () => {
       section: 'One',
       anchor: 'one',
       number: '01',
-      subtext: 'One',
       subblurb: 'Test Section One',
-      sectionTeaser: (
-        <div>
-          Section One Teaser
-        </div>
-      ),
-      blurb: (
-        <div>
-          Section One Blurb
-        </div>
-      ),
-      tagName: 'one',
-      readMoreOnMobile: true,
-    }
+    },
+    {
+      section: 'Two',
+      anchor: 'two',
+      number: '02',
+      subblurb: 'Test Section Two',
+      isNew: true
+    },
+    {
+      section: 'Three',
+      anchor: 'three',
+      number: '03',
+      subblurb: 'Test Section Three',
+      comingSoon: true
+    },
   ]
 
   const renderComponent = () =>
@@ -31,27 +32,31 @@ describe('Toc', () => {
       <Toc sections={testSections}/>
     )
 
-  it('dummy test', () => {
+  it('creates a div and grid element for each member of the array in the' +
+    ' sections prop', () => {
     const {getByText} = renderComponent({testSections});
     expect(getByText('One')).toBeTruthy();
-
-    screen.debug();
-  })
-
-  it('creates a div or grid element for each member of the array in the' +
-    ' sections prop', () => {
+    expect(getByText('Two')).toBeTruthy();
+    expect(getByText('Three')).toBeTruthy();
   });
 
-  it('shows a New message if the section has isNew = true', () => {
-
+  it('shows a "New" message if the section has prop isNew', () => {
+    const {getByText} = renderComponent({testSections});
+    expect(getByText('New')).toBeTruthy();
   });
 
-  it('shows a Coming Soon message is the section has comingSoon = true', () => {
-
+  it('shows a "Coming Soon!" message if the section has prop comingSoon', () => {
+    const {getByText} = renderComponent({testSections});
+    expect(getByText('Coming Soon!')).toBeTruthy();
   });
 
-  it('contains a clickable link for the entire section in the toc', () => {
-
+  it('contains a clickable link for each active section in the toc', () => {
+    const {getByText} = renderComponent({testSections});
+    const three = getByText('Three');
+    expect(screen.getAllByRole('link')[0]).toHaveAttribute('href', '#section-one');
+    expect(screen.getAllByRole('link')[1]).toHaveAttribute('href', '#section-two');
+    // but not for coming soon section
+    expect(three).not.toHaveAttribute('href', '#section-three');
   });
 
   it('does not display the sub-title at mobile size', () => {
