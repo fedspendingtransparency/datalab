c9e1c7030e7df92587adaa1c74fbb544b203e23e
brianpetway <brian.petway@kc.frb.org>
2021-03-11 12:08:37 -0600

    7877- wip; added first test, problem with jest reading the glossary csv file

--- src/components/headers/page/page.spec.js
+++ src/components/headers/page/page.spec.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import PageHeader from "./page";
 
 describe('PageHeader', () => {
   /* needs to receive several props
@@ -11,4 +12,76 @@ describe('PageHeader', () => {
      also an isHome prop
 
    */
+
+  const headerItems=['Analyses', "America's Finance Guide", 'Resources', 'About' +
+      ' Us', 'Glossary'];
+  const megamenuItems = [
+    {
+      analyses: [
+        { name: 'COVID-19 Funding', link: '/federal-covid-funding/' },
+        { name: 'Federal Account Explorer', link: '/federal-account-explorer/' },
+      ]
+    },
+    {
+      ffg: [
+        { header: 'Overview', name: "America's Finance Guide", link: '/americas-finance-guide/' },
+      ]
+    },
+    {
+      resources: [
+        { name: 'Analyst Guide', link: '/analyst-guide/' },
+      ]
+    },
+    {
+      about: [
+        { name: 'About Us', link: '/about/' },
+      ],
+    },
+    {
+      glossary: [
+        { name: 'Glossary', link: '#' },
+      ],
+    },
+  ];
+
+  const renderComponent = () =>
+    render(
+      <PageHeader headerItems={headerItems}
+                  megamenuItems={megamenuItems}
+                  isHome={true}
+      />
+    )
+
+  it('shows the five things in the nav', () => {
+    const { getByText } = renderComponent({});
+
+    expect(getByText('Analyses')).toBeTruthy();
+  });
+
+  it('shows the logo with tagline when on home page', () => {
+
+  });
+
+  it('shows the logo without tagline if not on home page', () => {
+
+  });
+
+  it('makes the navbar sticky when not on home page', () => {
+
+  });
+
+  it('adds classes to make the nav skinnier when not on home page', () => {
+    // styles.skinnyTop
+    // styles.tight
+    // styles.skinnySub
+  });
+
+  it('shows the burger button when width is < 992', () => {
+
+  });
+
+  it('uses the mobile version of menu when width is < 475', () => {
+
+  });
+
 })
